aeedfa985475bd94ab77c8a3886fdd0519cdfa28
James Anderson <jdande@me.com>
2019-11-23 20:23:19 -0800

    Initial Commit

---
+++ Just Chillin'Tests/Just_Chillin_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Just_Chillin_Tests.swift
+//  Just Chillin'Tests
+//
+//  Created by James Anderson on 11/23/19.
+//  Copyright © 2019 Mary Jane & Company, LLC. All rights reserved.
+//
+
+import XCTest
+@testable import Just_Chillin_
+
+class Just_Chillin_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
